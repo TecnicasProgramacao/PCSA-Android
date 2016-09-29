0755c5c505499c3de77387ce4e19514771e22203
Marcelo Augusto <mekmay@hotmail.com>
2016-09-29 17:25:26 -0300

    Adding assert and headers of methods and file in Resources.java

--- app/src/main/java/com/peacecorps/pcsa/reporting/Resources.java
+++ app/src/main/java/com/peacecorps/pcsa/reporting/Resources.java
@@ -1,3 +1,8 @@
+/**
+ * File: Resources.java
+ * Purpose: Resources for users to learn more about Reporting
+ */
+
 package com.peacecorps.pcsa.reporting;
 
 import android.app.Activity;
@@ -5,16 +10,15 @@ import android.os.Bundle;
 
 import com.peacecorps.pcsa.R;
 
-/**
- * Resources for users to learn more about Reporting
- *
- * @author Buddhiprabha Erabadda
- * @since 07-08-2015
- */
 public class Resources extends Activity {
 
+    /**
+     * Called when the activity is starting.
+     * @param savedInstanceState -It contains data provided by the activity previously initialized
+     */
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
+        assert savedInstanceState != null;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporting_resources);
