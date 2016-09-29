d7e55f7078eb9d44a0cd35f114b09b1a1eed2e3d
Marcelo Augusto <mekmay@hotmail.com>
2016-09-29 19:45:22 -0300

    Extracting methods onCreateView, adding headers and asserts the class and methods

--- app/src/main/java/com/peacecorps/pcsa/policies_glossary/PoliciesFragment.java
+++ app/src/main/java/com/peacecorps/pcsa/policies_glossary/PoliciesFragment.java
@@ -1,3 +1,8 @@
+/**
+ * File: PoliciesFragment.java
+ * Purpose: Where the screens that show more information about the project glossary and policies
+ */
+
 package com.peacecorps.pcsa.policies_glossary;
 
 import android.os.Bundle;
@@ -13,27 +18,50 @@ import com.peacecorps.pcsa.MainActivity;
 import com.peacecorps.pcsa.R;
 import com.peacecorps.pcsa.SingleTextViewFragment;
 
-/*
- * Policies and Glossary, Main Screen
- * @author rohan
- * @since 2016-07-24
- */
 public class PoliciesFragment extends Fragment {
 
     public static final String TAG = PoliciesFragment.class.getSimpleName();
-    private Button policyButton, glossaryButton, furtherButton;
+    private Button policyButton = null, glossaryButton = null, furtherButton = null;
+    private static View rootView = null;
+
+    /**
+     * Creates and returns the view hierarchy associated with the fragment.
+     * @param inflater - Object used to inflate any views in the fragment
+     * @param container - If non-null, is the parent view that the fragment should be attached to
+     * @param savedInstanceState - If non-null, this fragment is being re-constructed from a
+     *                           previous saved state as given here
+     * @return View - View of the fragment
+     */
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater,
                              final @Nullable ViewGroup container,
                              final @Nullable Bundle savedInstanceState) {
 
-        View rootView =  inflater.inflate(R.layout.fragment_policies, container, false);
+        assert inflater != null;
+
+        rootView =  inflater.inflate(R.layout.fragment_policies, container, false);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.policies_glossary);
+
+        setButtons();
+        setOnClickListeners();
+
+        return rootView;
+    }
+
+    /**
+     * Sets the Buttons of the PoliciesFragment view
+     */
+    private void setButtons() {
         policyButton = (Button) rootView.findViewById(R.id.policiesButton);
         glossaryButton = (Button) rootView.findViewById(R.id.glossaryButton);
         furtherButton = (Button) rootView.findViewById(R.id.furtherButton);
+    }
 
+    /**
+     * Sets the onClick listeners of the Buttons of the PoliciesFragment view
+     */
+    private void setOnClickListeners() {
         policyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
@@ -65,8 +93,6 @@ public class PoliciesFragment extends Fragment {
                         true);
             }
         });
-
-        return rootView;
     }
 }
 
