f8adffa023da070906a50636989a9a86b041fbdb
Marcelo Augusto <mekmay@hotmail.com>
2016-09-29 13:43:34 -0300

    Adding header of method onActivityResult in CircleOfTrustFragment

--- app/src/main/java/com/peacecorps/pcsa/circle_of_trust/CircleOfTrustFragment.java
+++ app/src/main/java/com/peacecorps/pcsa/circle_of_trust/CircleOfTrustFragment.java
@@ -490,6 +490,18 @@ public class CircleOfTrustFragment extends Fragment {
         return false;
     }
 
+    /**
+     * Receive the result from a previous call to
+     * {@link #startActivityForResult(Intent, int)}.
+     * @param requestCode - The integer request code originally supplied to
+     *                    startActivityForResult(), allowing you to identify who this
+     *                    result came from.
+     * @param resultCode - The integer result code returned by the child activity
+     *                   through its setResult().
+     * @param data - An Intent, which can return result data to the caller
+     *               (various data can be attached to Intent "extras").
+     */
+
     @Override
     public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
 
