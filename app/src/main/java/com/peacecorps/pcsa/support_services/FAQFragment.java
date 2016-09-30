805e053ebd2cdcf95a0558482f4646d433424f8b
Marcelo Augusto <mekmay@hotmail.com>
2016-09-29 23:45:53 -0300

    Adding asserts, headers in class and methods and initialization of variables in FAQFragment

--- app/src/main/java/com/peacecorps/pcsa/support_services/FAQFragment.java
+++ app/src/main/java/com/peacecorps/pcsa/support_services/FAQFragment.java
@@ -1,3 +1,8 @@
+/**
+ * File: FAQFragment.java
+ * Purpose: Frequently Asked Questions (FAQ) page implemented as a list
+ */
+
 package com.peacecorps.pcsa.support_services;
 
 import android.os.Bundle;
@@ -10,35 +15,46 @@ import android.widget.TextView;
 
 import com.peacecorps.pcsa.R;
 
-/**
- * Frequently Asked Questions (FAQ) page implemented as a list
- *
- * @author Buddhiprabha Erabadda
- * @since 07-08-2015
- */
 public class FAQFragment extends ListFragment {
 
-    public final static String TAG = FAQFragment.class.getSimpleName();
+    public static final String TAG = FAQFragment.class.getSimpleName();
+    private String[] values = null;
+    private String[] titles = null;
 
+    /**
+     * Creates and returns the view hierarchy associated with the fragment.
+     * @param inflater - Object used to inflate any views in the fragment
+     * @param container - If non-null, is the parent view that the fragment should be attached to
+     * @param savedInstanceState - If non-null, this fragment is being re-constructed from a
+     *                           previous saved state as given here
+     * @return View - View of the fragment
+     */
     @Override
     public View onCreateView(final LayoutInflater inflater,
                              final ViewGroup container,
                              final Bundle savedInstanceState) {
 
+        assert inflater != null;
+        assert container != null;
+        assert savedInstanceState != null;
+
         View rootView =  inflater.inflate(R.layout.fragment_coping, container, false);
         TextView subtitle = (TextView) rootView.findViewById(R.id.subtitle);
         subtitle.setText(getString(R.string.title_activity_faq));
 
-        String[] values = new String[] {
-                getResources().getString(R.string.reporting_faq1),
-                getResources().getString(R.string.reporting_faq2),
-                getResources().getString(R.string.reporting_faq3),
-                getResources().getString(R.string.reporting_faq4),
-                getResources().getString(R.string.reporting_faq5),
-                getResources().getString(R.string.reporting_faq6)
-        };
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.reporting_types);
 
-        String[] titles = new String[]{
+        FAQArrayAdapter faqArrayAdapter = new FAQArrayAdapter(getActivity(), titles, values);
+        setListAdapter(faqArrayAdapter);
+
+        return rootView;
+    }
+
+    /**
+     * Set titles in the resources of the fragment
+     */
+    private void settingTitles() {
+        titles = new String[]{
                 getResources().getString(R.string.reporting_faq1_header),
                 getResources().getString(R.string.reporting_faq2_header),
                 getResources().getString(R.string.reporting_faq3_header),
@@ -46,10 +62,19 @@ public class FAQFragment extends ListFragment {
                 getResources().getString(R.string.reporting_faq5_header),
                 getResources().getString(R.string.reporting_faq6_header)
         };
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.reporting_types);
+    }
 
-        FAQArrayAdapter faqArrayAdapter = new FAQArrayAdapter(getActivity(), titles, values);
-        setListAdapter(faqArrayAdapter);
-        return rootView;
+    /**
+     * Set values in the resources of the fragment
+     */
+    private void settingValues() {
+        values = new String[] {
+                getResources().getString(R.string.reporting_faq1),
+                getResources().getString(R.string.reporting_faq2),
+                getResources().getString(R.string.reporting_faq3),
+                getResources().getString(R.string.reporting_faq4),
+                getResources().getString(R.string.reporting_faq5),
+                getResources().getString(R.string.reporting_faq6)
+        };
     }
 }
