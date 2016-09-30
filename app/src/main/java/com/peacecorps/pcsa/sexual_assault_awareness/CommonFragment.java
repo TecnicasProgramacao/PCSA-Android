66e29ee775f2d25b9c08e57f974662e1702e3cad
Marcelo Augusto <mekmay@hotmail.com>
2016-09-29 23:13:22 -0300

    Adding headers in methods and class CommonFragment, adding asserts and initializations of variables

--- app/src/main/java/com/peacecorps/pcsa/sexual_assault_awareness/CommonFragment.java
+++ app/src/main/java/com/peacecorps/pcsa/sexual_assault_awareness/CommonFragment.java
@@ -1,3 +1,8 @@
+/**
+ * File: CommonFragment.java
+ * Purpose: This fragment displays the main questions sexual assault common questions
+ */
+
 package com.peacecorps.pcsa.sexual_assault_awareness;
 
 import android.os.Bundle;
@@ -12,43 +17,66 @@ import android.widget.TextView;
 import com.peacecorps.pcsa.R;
 import com.peacecorps.pcsa.support_services.FAQArrayAdapter;
 
-/*
- * Sexual Assault Common Questions
- * @author rohan
- * @since 2016-07-24
- */
 public class CommonFragment extends ListFragment {
 
     public static final String TAG = CommonFragment.class.getSimpleName();
+    private String[] values = null;
+    private String[] titles = null;
+
+    /**
+     * Creates and returns the view hierarchy associated with the fragment.
+     * @param inflater - Object used to inflate any views in the fragment
+     * @param container - If non-null, is the parent view that the fragment should be attached to
+     * @param savedInstanceState - If non-null, this fragment is being re-constructed from a
+     *                           previous saved state as given here
+     * @return View - View of the fragment
+     */
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater,
                              final @Nullable ViewGroup container,
                              final @Nullable Bundle savedInstanceState) {
 
+        assert inflater != null;
+
         View rootView =  inflater.inflate(R.layout.fragment_coping, container, false);
         TextView subtitle = (TextView) rootView.findViewById(R.id.subtitle);
         subtitle.setText(getString(R.string.common_questions));
 
-        String[] values = new String[] {
-                getResources().getString(R.string.common_as1),
-                getResources().getString(R.string.common_as2),
-                getResources().getString(R.string.common_as3),
-                getResources().getString(R.string.common_as4),
-                getResources().getString(R.string.common_as5)};
+        settingValues();
 
-        String[] titles = new String[]{
-                getResources().getString(R.string.common_qs_title1),
-                getResources().getString(R.string.common_qs_title2),
-                getResources().getString(R.string.common_qs_title3),
-                getResources().getString(R.string.common_qs_title4),
-                getResources().getString(R.string.common_qs_title5)};
+        settingTitles();
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.sexual_assault_awareness);
 
         FAQArrayAdapter faqArrayAdapter = new FAQArrayAdapter(getActivity(), titles, values);
         setListAdapter(faqArrayAdapter);
+
         return rootView;
+    }
+
+    /**
+     * Set titles in the resources of the fragment
+     */
+    private void settingTitles() {
+        titles = new String[]{
+                getResources().getString(R.string.common_qs_title1),
+                getResources().getString(R.string.common_qs_title2),
+                getResources().getString(R.string.common_qs_title3),
+                getResources().getString(R.string.common_qs_title4),
+                getResources().getString(R.string.common_qs_title5)};
+    }
+
+    /**
+     * Set values in the resources of the fragment
+     */
+    private void settingValues() {
+        values = new String[] {
+                getResources().getString(R.string.common_as1),
+                getResources().getString(R.string.common_as2),
+                getResources().getString(R.string.common_as3),
+                getResources().getString(R.string.common_as4),
+                getResources().getString(R.string.common_as5)};
     }
 }
 
